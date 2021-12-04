e3341ad52ec8aa39ffd964517e6aa8b7f460f8fa
miner <nomail>
2021-12-04 19:24:22 +0100

    improve nft recovery script

--- nft_recovery.sh
+++ nft_recovery.sh
@@ -17,20 +17,24 @@ then
 fi
 
 echo `date`
-
+echo "starting recovery per each coin"
 cd $HOME/fd-cli
 python3 -m venv venv
 source venv/bin/activate
 for coin in flora hddcoin staicoin flax stor; do
+{
   port=`yq e '.full_node.rpc_port' $HOME/.$coin/mainnet/config/config.yaml`
-  echo "${coin}": "${port}"
   export FD_CLI_BC_DB_PATH=$HOME/.$coin/mainnet/db/blockchain_v1_mainnet.sqlite
   export FD_CLI_WT_DB_PATH=$HOME/.$coin/mainnet/wallet/db/blockchain_wallet_v1_mainnet_2796152012.sqlite
-  fd-cli nft-recover \
+  OUTPUT=$(fd-cli nft-recover \
     -l $LAUNCHER_HASH \
     -p $POOL_CONTRACT_ADDRESS \
     -nh 0.0.0.0 \
     -np $port \
     -ct $HOME/.$coin/mainnet/config/ssl/full_node/private_full_node.crt \
-    -ck $HOME/.$coin/mainnet/config/ssl/full_node/private_full_node.key
+    -ck $HOME/.$coin/mainnet/config/ssl/full_node/private_full_node.key)
+  echo -e "${coin}:${port} \t $OUTPUT"
+} &
 done
+wait
+echo "all coins claimed"
