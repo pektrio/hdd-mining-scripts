84436b91926bd41ae1130c345729eca55d59fe69
miner <nomail>
2021-12-12 00:10:34 +0100

    read wallets in parallel

--- wallets.sh
+++ wallets.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 coins="aedge btcgreen chia hddcoin flax flora staicoin stor sit"
 for coin in $coins; do
+{
   #echo "starting $coin..."
   cd /home/miner/${coin}-blockchain/
   . ./activate
   OUTPUT=$(${coin} wallet show | grep '\-Total' | head -1)
   deactivate
-  echo "${OUTPUT}"
+  #echo "${coin}\t${OUTPUT}"
+  printf '%-10s: %s\n' "$coin" "$OUTPUT"
+} &
 done
+wait
