fd49c73d63bb25b8836775f8801b3ea01e692195
miner <nomail>
2021-12-15 18:16:42 +0100

    make startup script more flexible

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+mode=${1:-farmer}
 coins="aedge btcgreen chia hddcoin flax flora staicoin stor sit"
 for coin in $coins; do
   echo "starting $coin..."
-  screen -S $coin -dm bash -c "cd /home/miner/${coin}-blockchain/;. ./activate;${coin} start farmer; bash -c \"exec bash\""
+  screen -S $coin -dm bash -c "cd /home/miner/${coin}-blockchain/;. ./activate;${coin} start ${mode}; bash -c \"exec bash\""
+  sleep 1m
   echo "done"
 done
